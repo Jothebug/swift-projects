840cbbae42ddee2960e09667ee0bcfc7a033038d
Jothebug <hayenbg2603@gmail.com>
2024-10-03 22:53:07 +0700

    add project 06

---
+++ 06-TouristMap-V2/TouristMap/TouristMap/TouristMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouristMapApp.swift
+//  TouristMap
+//
+//  Created by HaYen on 3/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TouristMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
