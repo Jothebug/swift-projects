d58305cc636e9be970ceadc512ee6f3980b1f676
Jothebug <hayenbg2603@gmail.com>
2024-10-03 23:17:03 +0700

    implement Location model & Location Data

---
+++ 06-TouristMap-V2/TouristMap/TouristMap/Models/Location.swift
@@ -0,0 +1,19 @@
+//
+//  Location.swift
+//  TouristMap
+//
+//  Created by HaYen on 3/10/24.
+//
+
+import Foundation
+import MapKit
+
+struct Location: Identifiable {
+	let id: String = UUID().uuidString
+	let name: String
+	let cityName: String
+	let coordinates: CLLocationCoordinate2D
+	let description: String
+	let imageNames: [String]
+	let link: String
+}
