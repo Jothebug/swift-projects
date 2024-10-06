c8cdb915bd25d9819f311224b623b1384a546c0f
Jothebug <hayenbg2603@gmail.com>
2024-10-06 22:12:18 +0700

    add header

--- 06-TouristMap-V2/TouristMap/TouristMap/Views/HomeScreen.swift
+++ 06-TouristMap-V2/TouristMap/TouristMap/Views/HomeScreen.swift
@@ -9,21 +9,34 @@ import SwiftUI
 import MapKit
 
 struct HomeScreen: View {
-    
     @EnvironmentObject private var vm: LocationsViewModel
     
     var body: some View {
         ZStack {
-            Map(
-                coordinateRegion: $vm.mapRegion,
-                annotationItems: vm.locations,
-                annotationContent: { location in
-                    MapAnnotation(coordinate: location.coordinates) {
-                        LocationAnnotation()
-                            .scaleEffect(vm.mapLocation == location ? 1 : 0.7)
-                    }
+            mapLayer.ignoresSafeArea()
+            VStack {
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Text("Test location")
+                        .font(.title2)
+                        .fontWeight(.black)
+                        .foregroundColor(.primary)
+                        .frame(height: 55)
+                        .frame(maxWidth: .infinity)
+                        .overlay {
+                            Image(systemName: "arrow.down")
+                                .font(.headline)
+                                .foregroundColor(.primary)
+                                .padding()
+                                .rotationEffect(Angle(degrees:180))
+                        }
                 })
-                .ignoresSafeArea()
+            }
+            .background(.thickMaterial)
+            .cornerRadius(10)
+            .shadow(
+                color: Color.black.opacity(0.3), radius: 20,
+                x: 0, y: 15
+            )
         }
     }
 }
@@ -31,4 +44,18 @@ struct HomeScreen: View {
 #Preview {
     HomeScreen()
         .environmentObject(LocationsViewModel())
+}
+
+extension HomeScreen {
+    private var mapLayer: some View {
+        Map(
+            coordinateRegion: $vm.mapRegion,
+            annotationItems: vm.locations,
+            annotationContent: { location in
+                MapAnnotation(coordinate: location.coordinates) {
+                    LocationAnnotation()
+                        .scaleEffect(vm.mapLocation == location ? 1 : 0.7)
+                }
+            })
+    }
 }
