2806eb315a39d06cb71fb8e17f6da4886f99144a
Jothebug <hayenbg2603@gmail.com>
2024-10-08 22:10:11 +0700

    implement LocationPreviewUI

---
+++ 06-TouristMap-V2/TouristMap/TouristMap/Views/LocationPreview.swift
@@ -0,0 +1,18 @@
+//
+//  LocationPreview.swift
+//  TouristMap
+//
+//  Created by HaYen on 8/10/24.
+//
+
+import SwiftUI
+
+struct LocationPreview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationPreview()
+}
