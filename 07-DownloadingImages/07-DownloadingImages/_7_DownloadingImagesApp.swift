b52bef82e7ec70845b9e613c10aee02b01e50f3f
Jothebug <hayenbg2603@gmail.com>
2024-11-03 13:21:17 +0700

    add 07 project

---
+++ 07-DownloadingImages/07-DownloadingImages/_7_DownloadingImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  _7_DownloadingImagesApp.swift
+//  07-DownloadingImages
+//
+//  Created by HaYen on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct _7_DownloadingImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
